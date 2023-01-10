bb20d68af6ba09ad70fbb202149a50f6c9e5adac
Ameerusa86 <engamermecha@gmail.com>
2023-01-09 23:39:56 -0500

    -

--- Movies/Movies/src/api/api.js
+++ Movies/Movies/src/api/api.js
@@ -1,8 +1,9 @@
 const API_URL = {
-  base_url: "https://api.themoviedb.org/3/",
-  IMG_W500: `https://image.tmdb.org/t/p/w500/${imgPath}`,
-  IMG_Original: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-  API_KEY: (imgPath) => "4ba85140a3815a44c77d9273f825f43b",
+  base_url:
+    "https://api.themoviedb.org/3/movie/popular?api_key=4a1414e6b1a6bd74ff7f45f4b0a63770",
+  IMG_Original: "https://image.tmdb.org/t/p/original/",
+  IMG_W500: "https://image.tmdb.org/t/p/w500/",
+  // API_KEY "4ba85140a3815a44c77d9273f825f43b",
 };
 
-export default API_KEY;
+export default API_URL;
--- Movies/Movies/src/components/header/Header.jsx
+++ Movies/Movies/src/components/header/Header.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API_URL from "../../api/api";
 
 const Header = () => {
+  const [movies, setMovies] = useState([]);
+
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  // console.log(movie);
+
+  useEffect(() => {
+    fetch(API_URL.base_url)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  }, []);
+
+  console.log(movies);
   return (
-    <div className=" w-screen h-[600px] bg-blue-300">
+    <div className="relative w-full h-[600px]">
       <img
-        className="object-contain sm:w-full sm:h-full sm:object-cover"
-        src="src\assets\images\cropped-3840-2160-724132.jpg"
-        alt=""
+        className="w-full h-full object-cover"
+        src={API_URL.IMG_Original + movie?.poster_path}
       />
+      <div>
+        <h1 className="absolute top-80 left-10 text-2xl text-white  py-2 px-4 bg-orange-600 rounded-lg">
+          {movie?.release_date}
+        </h1>
+        <h1 className="absolute top-80 left-10 text-2xl text-white  py-2 px-4 bg-orange-600 rounded-lg">
+          {movie?.title}
+        </h1>
+      </div>
     </div>
   );
 };
